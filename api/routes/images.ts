0baea577f83477ade7556e83f81669a28dd92625
DHjeelm <dnl1@live.se>
2022-11-22 16:20:46 +0100

    Removed redundant code

--- api/routes/images.ts
+++ api/routes/images.ts
@@ -3,37 +3,43 @@ import console from "console";
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
-import {GridFsStorage} from 'multer-gridfs-storage'
+import { GridFsStorage } from "multer-gridfs-storage";
 
 // Old implementation, saved for reference
 // const upload = require("../middleware/upload");
 
- // module.exports = multer({ storage: storage });
+// module.exports = multer({ storage: storage });
 
- // Schema
+// Schema
 // import Image from "../models/images";
-
 
 // GridFS
 const Grid = require("gridfs-stream");
 let gfs: any, gridfsBucket: any;
 
+// Mongoose connection
 const conn = mongoose.connection;
 conn.once("open", () => {
   gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
-    bucketName: 'images'
+    bucketName: "images",
   });
- 
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('images');
- })
+
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("images");
+});
 
 // GridFS storage, called in post request
 const storage = new GridFsStorage({
-  url: "mongodb+srv://admin:" +
-  "hejhej" +
-  "@reindeerlichens.ro1gjeu.mongodb.net/?retryWrites=true&w=majority",
+  // URL
+  url:
+    "mongodb+srv://admin:" +
+    "hejhej" +
+    "@reindeerlichens.ro1gjeu.mongodb.net/?retryWrites=true&w=majority",
+
+  // Options
   options: { useNewUrlParser: true, useUnifiedTopology: true },
+
+  // Handle file upload
   file: async (req: any, file: any) => {
     const match = ["image/png", "image/jpeg"];
 
@@ -43,137 +49,148 @@ const storage = new GridFsStorage({
     }
     // Loop through the files
     try {
-      await gfs.collection('images').find().toArray((err: any, files: any) => {
-        files.forEach((fileInDB:any) => {
-          // Check if the file is already in the database
-          if (fileInDB.filename === file.originalname) {
-            console.log("File already exists, old file will be overwritten");
-            // Delete the file from the database
-            gridfsBucket.delete(new mongoose.Types.ObjectId(fileInDB._id), (err: any) => {
-              console.log("File deleted");
-              if (err) {
-                console.log(err);
-              }
-            });
-          }
-          
+      await gfs
+        .collection("images")
+        .find()
+        .toArray((err: any, files: any) => {
+          files.forEach((fileInDB: any) => {
+            // Check if the file is already in the database
+            if (fileInDB.filename === file.originalname) {
+              console.log("File already exists, old file will be overwritten");
+              // Delete the file from the database
+              gridfsBucket.delete(
+                new mongoose.Types.ObjectId(fileInDB._id),
+                (err: any) => {
+                  console.log("File deleted");
+                  if (err) {
+                    console.log(err);
+                  }
+                }
+              );
+            }
+          });
         });
-      });
       // Add new file to database
       return {
         bucketName: "images",
         filename: `${file.originalname}`,
-      }
-
+      };
+      // Catch error
     } catch (err) {
-      console.log(err)
-      
+      console.log(err);
     }
+  },
+});
 
-    
-
-  },});
-
-  // Multer middleware
-  const upload = multer({
-    storage
-  });
-
- 
+// Multer/GridFS middleware
+const upload = multer({
+  storage,
+});
 
 // Router
 const router = express.Router();
 
-
-
-// Get all images
+// Get information about all images in DB
 router.get("/", async (req, res, next) => {
-  try{
+  // Try to find images in database
+  try {
     await gridfsBucket.find().toArray((err: any, files: any) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: "No files exist",
-      });
-      
-    } else {
-      // Files exist
-      return res.status(200).json({
-        numberOfImages: files.length,
-        images: files,
+      // Check if files exists
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "No files exist",
+        });
+      } else {
+        // Files exist, return file information and number of files
+        return res.status(200).json({
+          numberOfImages: files.length,
+          images: files.map((file: any) => {
+            // Do we want to see more? More attributes: length, chunkSize, uploadDate etc.
+            return {
+              _id: file._id,
+              filename: file.filename,
+            };
+          }),
+        });
       }
-      );
-
-    }
-  });
+    });
+    // Catch errors
   } catch (err) {
     console.log(err);
     res.status(500).json({
       error: err,
     });
   }
- 
 });
 
-// Get one image by name
+// Fetch one image by name
 router.get("/:imageName", async (req, res) => {
-  try{
-    await gfs.files.findOne({filename: req.params.imageName}, (err:any, file:any) => {
-        if(!file || file.length === 0){
-            return res.status(404).json({err: 'No File Exists'});
+  try {
+    // Find file in database
+    await gfs.files.findOne(
+      { filename: req.params.imageName },
+      (err: any, file: any) => {
+        if (!file || file.length === 0) {
+          // File not found
+          return res.status(404).json({ err: "No File Exists" });
         } else {
-            // Check if is image
-            if(file.contentType === "image/jpeg" || file.contentType === "image/png"){
-                // Read output to broswer
-                const readStream = gridfsBucket.openDownloadStream(file._id);
-                readStream.pipe(res);
-            } else {
-                res.status(404).json({err: 'Not and image'});
-            }
+          // File found, return file
+          const readStream = gridfsBucket.openDownloadStream(file._id);
+          readStream.pipe(res);
         }
-    })
+      }
+    );
   } catch (err) {
-    res.status(500).json({err: err});
+    res.status(500).json({ err: err });
   }
 });
 
-// Post
-router.post("/", upload.single('file'), (req, res) => {
+// Post request, upload image
+router.post("/", upload.single("file"), (req, res) => {
   // console.log(req.file);
   if (req.file == undefined) {
     return res.status(400).send({
-      message: "Please upload a file!"});
+      message: "Please upload a file!",
+    });
   }
   res.status(200).send({
     message: "Uploaded the file successfully: " + req.file.originalname,
   });
 });
 
-
-// Delete
+// Delete image by name
 router.delete("/:imageName", async (req, res, next) => {
-  try{
+  try {
     // Check if image exists
-    await gfs.files.findOne({filename: req.params.imageName}, (err:any, file:any) => {
-        if(!file || file.length === 0){
-            return res.status(404).json({err: 'No file Exists'});
+    await gfs.files.findOne(
+      { filename: req.params.imageName },
+      (err: any, file: any) => {
+        if (!file || file.length === 0) {
+          return res
+            .status(404)
+            .json({ err: "No file with that name exists, please try again!" });
         } else {
-              // Delete image
-              gridfsBucket.delete(new mongoose.Types.ObjectId(file._id), (err: any) => {
-                if(err){
-                    return res.status(500).json({err: err});
-                } else {
-                    res.status(200).json({message: 'File deleted'});
-                }
-              });
-          
+          // Delete image
+          gridfsBucket.delete(
+            new mongoose.Types.ObjectId(file._id),
+            (err: any) => {
+              if (err) {
+                return res.status(500).json({ err: err });
+              } else {
+                res.status(200).json({
+                  message: "File successfully deleted from database",
+                  file: file,
+                });
+              }
+            }
+          );
         }
-    })
+      }
+    );
+    // Catch errors
   } catch (err) {
-    res.status(500).json({err: err});
+    res.status(500).json({ err: err });
   }
 });
-
-
 
 export default router;
