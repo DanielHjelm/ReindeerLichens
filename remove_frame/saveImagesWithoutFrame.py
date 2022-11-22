8beeddc5adeb52767bc1f376cfd2c938aea7c009
DHjeelm <dnl1@live.se>
2022-11-22 17:06:34 +0100

    Fixed print statement and logic error

--- remove_frame/saveImagesWithoutFrame.py
+++ remove_frame/saveImagesWithoutFrame.py
@@ -8,7 +8,7 @@ from PIL import Image
 def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images_to_save):
     # Loop through all the images in the folder
     for i, image in enumerate(os.listdir(os.getcwd()+'/'+path_to_images)):
-        if i > number_of_images_to_save:
+        if i+1 > number_of_images_to_save:
             break
             
         # Remove frame 
@@ -20,9 +20,11 @@ def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images
         # Save image to correct folder
         matplotlib.image.imsave(path_to_save_folder+'/'+os.path.splitext(image)[0]+'.png', without_frame)
 
+        print("Saved image: ", os.path.splitext(image)[0]+'.png')
+
 
 if __name__ == "__main__":
     path_to_images = 'blue'
     path_to_save_folder = 'withoutFrame'
-    number_of_images_to_save = 10
+    number_of_images_to_save = 5
     saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images_to_save)
