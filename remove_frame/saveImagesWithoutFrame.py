5c7be709e08fe6b65568feacce3c14db2fd6b518
DHjeelm <dnl1@live.se>
2022-11-30 13:17:39 +0100

    Updated so we save the image in its original format

--- remove_frame/saveImagesWithoutFrame.py
+++ remove_frame/saveImagesWithoutFrame.py
@@ -10,6 +10,8 @@ def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images
     for i, image in enumerate(os.listdir(os.getcwd()+'/'+path_to_images)):
         if i+1 > number_of_images_to_save:
             break
+
+        print(image)
             
         # Remove frame 
         without_frame = removeFrame(os.getcwd()+'/'+path_to_images+'/'+image)
@@ -18,13 +20,15 @@ def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images
         without_frame = cv2.cvtColor(without_frame, cv2.COLOR_BGR2RGB)
     
         # Save image to correct folder
-        matplotlib.image.imsave(path_to_save_folder+'/'+os.path.splitext(image)[0]+'.png', without_frame)
+        matplotlib.image.imsave(path_to_save_folder+'/'+image, without_frame)
+        # im = Image.fromarray(without_frame)
+        # im.save(path_to_save_folder+'/'+os.path.splitext(image)[0]+'.png')
 
         print("Saved image: ", os.path.splitext(image)[0]+'.png')
 
 
 if __name__ == "__main__":
-    path_to_images = 'blue'
+    path_to_images = '../../imagesToDB'
     path_to_save_folder = 'withoutFrame'
     number_of_images_to_save = 5
     saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images_to_save)
