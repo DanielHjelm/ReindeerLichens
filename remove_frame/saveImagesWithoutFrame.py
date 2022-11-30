6d775c98b372dc4f0ba027ae5d2c35ea3eb1a6cf
DHjeelm <dnl1@live.se>
2022-11-30 13:29:14 +0100

    Updated so we save the image in its original format

--- remove_frame/saveImagesWithoutFrame.py
+++ remove_frame/saveImagesWithoutFrame.py
@@ -10,19 +10,16 @@ def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images
     for i, image in enumerate(os.listdir(os.getcwd()+'/'+path_to_images)):
         if i+1 > number_of_images_to_save:
             break
-        
+
         # Remove frame 
         without_frame = removeFrame(os.getcwd()+'/'+path_to_images+'/'+image)
-        
-        # Convert to correct colors
-        without_frame = cv2.cvtColor(without_frame, cv2.COLOR_BGR2RGB)
-    
-        # Save image to correct folder
-        matplotlib.image.imsave(path_to_save_folder+'/'+image, without_frame)
-        # im = Image.fromarray(without_frame)
-        # im.save(path_to_save_folder+'/'+os.path.splitext(image)[0]+'.png')
 
-        print("Saved image: ", os.path.splitext(image)[0]+'.png')
+        # Make sure the frame is removed
+        if without_frame is not None:
+            # Save image
+            cv2.imwrite(path_to_save_folder+'/'+image, without_frame)
+            print(f"Saved image: {image}")
+    
 
 
 if __name__ == "__main__":
