c112f235a78d76f281dfc8444b415c6581f57b2a
JohRid <johannesridefelt@hotmail.com>
2022-12-01 11:44:22 +0100

    fixa i en mapp

---
+++ color_correction/remove_frame/removeBlueFrame.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def removeBlueFrame(image_path):
+    '''Function that removes the frame in the image'''
+
+    # Load image
+    img = cv2.imread(image_path)
+
+
+    # Create grayscale image
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # GaussionBlur the gray image
+    kernel_size = 3
+    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+    # Threshold image
+    thresh = cv2.threshold(blur_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Create kernel used in the dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13,13))
+
+    # Dilate the thresholded image
+    dilate = cv2.dilate(thresh, kernel, iterations=3)
+
+    # Median filter to remove noise
+    median = cv2.medianBlur(dilate, 21)
+
+    # Fill holes in frame with vertical and horizontal opening
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT,(500,1))
+    res = cv2.morphologyEx(median,cv2.MORPH_OPEN,kernel2)
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT,(1,500))
+    res = cv2.morphologyEx(res,cv2.MORPH_OPEN,kernel2)
+
+    # Find the contours in the image
+    contours = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    # Loop through each area
+    area_list = []
+    for i,contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        area_list.append((i,area))
+
+    # Find maximum area
+    max_tuple = max(area_list, key=lambda tup: tup[1])
+
+    # Use boundingRect to find area within frame
+    x,y,w,h = cv2.boundingRect(contours[max_tuple[0]])
+
+    # Area within frame
+    output = np.zeros((w,h))
+    output = img[y:y+h, x:x+w]
+
+    return output, x, y, w, h
+
+if __name__ == "__main__":
+    image = removeBlueFrame('IMG_0155.jpg')
+    plt.imshow(image)
+    plt.show()
+
+
---
+++ color_correction/remove_frame/removeFrame.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from removeBlueFrame import removeBlueFrame
+from removeFoldingRule import removeFoldingRule
+
+
+def removeFrame(image_path):
+    '''Function for removing frame from image'''
+    
+    # Read image
+    img = cv2.imread(image_path)
+    
+    # Convert to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+
+    # Create mask for blue color in HSV
+    lower = (80,100,100)
+    upper = (160,255,255)
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Count non-zero pixels in mask (i.e blue pixels)
+    count=np.count_nonzero(mask)
+    
+    # If images have to few blue pixels, use removeFoldingRule
+    if count < 333000:
+        return removeFoldingRule(image_path)
+    
+    # Else use, removeBlueFrame
+    else:
+        return removeBlueFrame(image_path)
