86150e9f29ef46ce007467b7fd546e532c5b3223
Mannewreeby <manne.wreeby@gmail.com>
2022-12-01 14:01:12 +0100

    Changed color of ok symbol

--- grow_cut_golang/web_server/pages/masks/[imageName].tsx
+++ grow_cut_golang/web_server/pages/masks/[imageName].tsx
@@ -176,7 +176,7 @@ export default function Mask({ mask, image, fileName }: { mask: string; image: s
     switch (requestStatus) {
       case "ok":
         return (
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-green-700">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-white">
             <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
         );
