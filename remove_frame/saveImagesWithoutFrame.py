5bae2a006c63951ca6f2f131c2052ebd92d08ae6
DHjeelm <dnl1@live.se>
2022-12-23 09:58:18 +0100

    Added comments and sys arguments

--- remove_frame/saveImagesWithoutFrame.py
+++ remove_frame/saveImagesWithoutFrame.py
@@ -1,15 +1,12 @@
 import cv2
-import numpy as np
-import matplotlib
 import os
 from removeFrame import removeFrame
-from PIL import Image 
+import sys
 
-def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images_to_save):
+
+def saveImagesWithoutFrame(path_to_images, path_to_save_folder):
     # Loop through all the images in the folder
     for i, image in enumerate(os.listdir(os.getcwd()+'/'+path_to_images)):
-        if i+1 > number_of_images_to_save:
-            break
 
         # Remove frame 
         without_frame = removeFrame(os.getcwd()+'/'+path_to_images+'/'+image)
@@ -20,10 +17,30 @@ def saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images
             cv2.imwrite(path_to_save_folder+'/'+image, without_frame)
             print(f"Saved image: {image}")
     
+def createFolders(outputFolders=["withoutFrame"]):
+    '''Create folders for images without frame'''
 
+    for folder in outputFolders:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
 if __name__ == "__main__":
-    path_to_images = '../../removeImgFrame'
-    path_to_save_folder = 'withoutFrame'
-    number_of_images_to_save = 1000
-    saveImagesWithoutFrame(path_to_images, path_to_save_folder, number_of_images_to_save)
+
+    if len(sys.argv) < 3:
+        print("USAGE: python3 evaluate.py <path_to_images> <path_to_save_folder>")
+
+        sys.exit(1)
+
+    # Set the paths to the ground truth and predictions
+    path_to_images = sys.argv[1]
+    path_to_save_folder = sys.argv[2]
+
+    if (not path_to_images) or (not path_to_save_folder):
+        print("No path to ground truth or predictions provided.")
+        sys.exit(1)
+
+    # Create folders
+    createFolders([path_to_save_folder])
+    
+   # Save images without frame
+    saveImagesWithoutFrame(path_to_images, path_to_save_folder)
